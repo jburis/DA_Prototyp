3169789f2355dfb7d456cd749157cd9398fcfd0f
Domele <adomele@student.tgm.ac.at>
2025-11-03 23:33:39 +0100

    Erstellung der Klassen für Admin Bereich

---
+++ app/src/main/java/com/example/da_prototyp_ocr/AdminLoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.da_prototyp_ocr;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+
+        EditText etUser = findViewById(R.id.et_user);
+        EditText etPass = findViewById(R.id.et_pass);
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(v -> {
+            String user = etUser.getText().toString();
+            String pass = etPass.getText().toString();
+            if (user.equals("Admin") && pass.equals("deinPasswort")) {
+                Toast.makeText(this, "Login erfolgreich!", Toast.LENGTH_SHORT).show();
+                // TODO: Admin-Dashboard öffnen
+            } else {
+                Toast.makeText(this, "Falsche Daten!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
